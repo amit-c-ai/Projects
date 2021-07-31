33cdcd1f04831d12294c6b24ff9184412208e90e
amit-c-ai <amitchoudhary9425@gmail.com>
2021-07-31 12:37:04 +0530

    Hash table storing project

---
+++ Hash Table Storing/build-qfiledemo-Desktop_Qt_5_12_10_MinGW_64_bit-Release/ui_mainwindow.h
@@ -0,0 +1,369 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.12.10
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPlainTextEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpinBox>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QGridLayout *gridLayout_18;
+    QGridLayout *gridLayout_11;
+    QGridLayout *gridLayout_10;
+    QGridLayout *gridLayout_3;
+    QComboBox *comboBox;
+    QPushButton *pushButton_2;
+    QGridLayout *gridLayout_2;
+    QLabel *label;
+    QSpinBox *spinBox;
+    QPushButton *pushButton;
+    QGridLayout *gridLayout_17;
+    QLabel *label_10;
+    QSpinBox *spinBox_2;
+    QPlainTextEdit *plainTextEdit_2;
+    QGridLayout *gridLayout_9;
+    QGridLayout *gridLayout;
+    QLabel *label_2;
+    QLineEdit *lineEdit;
+    QGridLayout *gridLayout_4;
+    QLabel *label_3;
+    QLineEdit *lineEdit_2;
+    QGridLayout *gridLayout_5;
+    QLineEdit *lineEdit_3;
+    QLabel *label_4;
+    QGridLayout *gridLayout_6;
+    QLabel *label_5;
+    QLineEdit *lineEdit_4;
+    QGridLayout *gridLayout_7;
+    QLabel *label_6;
+    QLineEdit *lineEdit_5;
+    QGridLayout *gridLayout_8;
+    QLabel *label_7;
+    QLineEdit *lineEdit_6;
+    QPushButton *pushButton_3;
+    QGridLayout *gridLayout_16;
+    QPlainTextEdit *plainTextEdit;
+    QGridLayout *gridLayout_15;
+    QGridLayout *gridLayout_14;
+    QLabel *label_11;
+    QLineEdit *lineEdit_9;
+    QGridLayout *gridLayout_12;
+    QLabel *label_8;
+    QLineEdit *lineEdit_7;
+    QGridLayout *gridLayout_13;
+    QLineEdit *lineEdit_8;
+    QLabel *label_9;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(662, 393);
+        MainWindow->setMinimumSize(QSize(572, 353));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        gridLayout_18 = new QGridLayout(centralwidget);
+        gridLayout_18->setObjectName(QString::fromUtf8("gridLayout_18"));
+        gridLayout_11 = new QGridLayout();
+        gridLayout_11->setObjectName(QString::fromUtf8("gridLayout_11"));
+        gridLayout_10 = new QGridLayout();
+        gridLayout_10->setObjectName(QString::fromUtf8("gridLayout_10"));
+        gridLayout_3 = new QGridLayout();
+        gridLayout_3->setObjectName(QString::fromUtf8("gridLayout_3"));
+        comboBox = new QComboBox(centralwidget);
+        comboBox->addItem(QString());
+        comboBox->addItem(QString());
+        comboBox->addItem(QString());
+        comboBox->addItem(QString());
+        comboBox->setObjectName(QString::fromUtf8("comboBox"));
+
+        gridLayout_3->addWidget(comboBox, 3, 0, 1, 1);
+
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+
+        gridLayout_3->addWidget(pushButton_2, 4, 0, 1, 1);
+
+        gridLayout_2 = new QGridLayout();
+        gridLayout_2->setObjectName(QString::fromUtf8("gridLayout_2"));
+        label = new QLabel(centralwidget);
+        label->setObjectName(QString::fromUtf8("label"));
+
+        gridLayout_2->addWidget(label, 0, 0, 1, 1);
+
+        spinBox = new QSpinBox(centralwidget);
+        spinBox->setObjectName(QString::fromUtf8("spinBox"));
+        spinBox->setValue(13);
+
+        gridLayout_2->addWidget(spinBox, 0, 1, 1, 1);
+
+
+        gridLayout_3->addLayout(gridLayout_2, 1, 0, 1, 1);
+
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+
+        gridLayout_3->addWidget(pushButton, 0, 0, 1, 1);
+
+        gridLayout_17 = new QGridLayout();
+        gridLayout_17->setObjectName(QString::fromUtf8("gridLayout_17"));
+        label_10 = new QLabel(centralwidget);
+        label_10->setObjectName(QString::fromUtf8("label_10"));
+
+        gridLayout_17->addWidget(label_10, 0, 0, 1, 1);
+
+        spinBox_2 = new QSpinBox(centralwidget);
+        spinBox_2->setObjectName(QString::fromUtf8("spinBox_2"));
+        spinBox_2->setValue(7);
+
+        gridLayout_17->addWidget(spinBox_2, 0, 1, 1, 1);
+
+
+        gridLayout_3->addLayout(gridLayout_17, 2, 0, 1, 1);
+
+
+        gridLayout_10->addLayout(gridLayout_3, 0, 1, 1, 1);
+
+        plainTextEdit_2 = new QPlainTextEdit(centralwidget);
+        plainTextEdit_2->setObjectName(QString::fromUtf8("plainTextEdit_2"));
+
+        gridLayout_10->addWidget(plainTextEdit_2, 0, 0, 1, 1);
+
+
+        gridLayout_11->addLayout(gridLayout_10, 0, 0, 1, 1);
+
+        gridLayout_9 = new QGridLayout();
+        gridLayout_9->setObjectName(QString::fromUtf8("gridLayout_9"));
+        gridLayout = new QGridLayout();
+        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
+        label_2 = new QLabel(centralwidget);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+
+        gridLayout->addWidget(label_2, 0, 0, 1, 1);
+
+        lineEdit = new QLineEdit(centralwidget);
+        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
+
+        gridLayout->addWidget(lineEdit, 1, 0, 1, 1);
+
+
+        gridLayout_9->addLayout(gridLayout, 0, 0, 1, 1);
+
+        gridLayout_4 = new QGridLayout();
+        gridLayout_4->setObjectName(QString::fromUtf8("gridLayout_4"));
+        label_3 = new QLabel(centralwidget);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+
+        gridLayout_4->addWidget(label_3, 0, 0, 1, 1);
+
+        lineEdit_2 = new QLineEdit(centralwidget);
+        lineEdit_2->setObjectName(QString::fromUtf8("lineEdit_2"));
+
+        gridLayout_4->addWidget(lineEdit_2, 1, 0, 1, 1);
+
+
+        gridLayout_9->addLayout(gridLayout_4, 0, 1, 1, 1);
+
+        gridLayout_5 = new QGridLayout();
+        gridLayout_5->setObjectName(QString::fromUtf8("gridLayout_5"));
+        lineEdit_3 = new QLineEdit(centralwidget);
+        lineEdit_3->setObjectName(QString::fromUtf8("lineEdit_3"));
+
+        gridLayout_5->addWidget(lineEdit_3, 1, 0, 1, 1);
+
+        label_4 = new QLabel(centralwidget);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+
+        gridLayout_5->addWidget(label_4, 0, 0, 1, 1);
+
+
+        gridLayout_9->addLayout(gridLayout_5, 0, 2, 1, 1);
+
+        gridLayout_6 = new QGridLayout();
+        gridLayout_6->setObjectName(QString::fromUtf8("gridLayout_6"));
+        label_5 = new QLabel(centralwidget);
+        label_5->setObjectName(QString::fromUtf8("label_5"));
+
+        gridLayout_6->addWidget(label_5, 0, 0, 1, 1);
+
+        lineEdit_4 = new QLineEdit(centralwidget);
+        lineEdit_4->setObjectName(QString::fromUtf8("lineEdit_4"));
+
+        gridLayout_6->addWidget(lineEdit_4, 1, 0, 1, 1);
+
+
+        gridLayout_9->addLayout(gridLayout_6, 0, 3, 1, 1);
+
+        gridLayout_7 = new QGridLayout();
+        gridLayout_7->setObjectName(QString::fromUtf8("gridLayout_7"));
+        label_6 = new QLabel(centralwidget);
+        label_6->setObjectName(QString::fromUtf8("label_6"));
+
+        gridLayout_7->addWidget(label_6, 0, 0, 1, 1);
+
+        lineEdit_5 = new QLineEdit(centralwidget);
+        lineEdit_5->setObjectName(QString::fromUtf8("lineEdit_5"));
+
+        gridLayout_7->addWidget(lineEdit_5, 1, 0, 1, 1);
+
+
+        gridLayout_9->addLayout(gridLayout_7, 0, 4, 1, 1);
+
+        gridLayout_8 = new QGridLayout();
+        gridLayout_8->setObjectName(QString::fromUtf8("gridLayout_8"));
+        label_7 = new QLabel(centralwidget);
+        label_7->setObjectName(QString::fromUtf8("label_7"));
+
+        gridLayout_8->addWidget(label_7, 0, 0, 1, 1);
+
+        lineEdit_6 = new QLineEdit(centralwidget);
+        lineEdit_6->setObjectName(QString::fromUtf8("lineEdit_6"));
+
+        gridLayout_8->addWidget(lineEdit_6, 1, 0, 1, 1);
+
+
+        gridLayout_9->addLayout(gridLayout_8, 0, 5, 1, 1);
+
+        pushButton_3 = new QPushButton(centralwidget);
+        pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
+
+        gridLayout_9->addWidget(pushButton_3, 0, 6, 1, 1);
+
+
+        gridLayout_11->addLayout(gridLayout_9, 1, 0, 1, 1);
+
+
+        gridLayout_18->addLayout(gridLayout_11, 0, 0, 1, 1);
+
+        gridLayout_16 = new QGridLayout();
+        gridLayout_16->setObjectName(QString::fromUtf8("gridLayout_16"));
+        plainTextEdit = new QPlainTextEdit(centralwidget);
+        plainTextEdit->setObjectName(QString::fromUtf8("plainTextEdit"));
+
+        gridLayout_16->addWidget(plainTextEdit, 0, 0, 1, 1);
+
+        gridLayout_15 = new QGridLayout();
+        gridLayout_15->setObjectName(QString::fromUtf8("gridLayout_15"));
+        gridLayout_14 = new QGridLayout();
+        gridLayout_14->setObjectName(QString::fromUtf8("gridLayout_14"));
+        label_11 = new QLabel(centralwidget);
+        label_11->setObjectName(QString::fromUtf8("label_11"));
+
+        gridLayout_14->addWidget(label_11, 0, 0, 1, 1);
+
+        lineEdit_9 = new QLineEdit(centralwidget);
+        lineEdit_9->setObjectName(QString::fromUtf8("lineEdit_9"));
+
+        gridLayout_14->addWidget(lineEdit_9, 0, 1, 1, 1);
+
+
+        gridLayout_15->addLayout(gridLayout_14, 0, 0, 1, 1);
+
+        gridLayout_12 = new QGridLayout();
+        gridLayout_12->setObjectName(QString::fromUtf8("gridLayout_12"));
+        label_8 = new QLabel(centralwidget);
+        label_8->setObjectName(QString::fromUtf8("label_8"));
+
+        gridLayout_12->addWidget(label_8, 0, 0, 1, 1);
+
+        lineEdit_7 = new QLineEdit(centralwidget);
+        lineEdit_7->setObjectName(QString::fromUtf8("lineEdit_7"));
+
+        gridLayout_12->addWidget(lineEdit_7, 0, 1, 1, 1);
+
+
+        gridLayout_15->addLayout(gridLayout_12, 1, 0, 1, 1);
+
+        gridLayout_13 = new QGridLayout();
+        gridLayout_13->setObjectName(QString::fromUtf8("gridLayout_13"));
+        lineEdit_8 = new QLineEdit(centralwidget);
+        lineEdit_8->setObjectName(QString::fromUtf8("lineEdit_8"));
+
+        gridLayout_13->addWidget(lineEdit_8, 0, 1, 1, 1);
+
+        label_9 = new QLabel(centralwidget);
+        label_9->setObjectName(QString::fromUtf8("label_9"));
+
+        gridLayout_13->addWidget(label_9, 0, 0, 1, 1);
+
+
+        gridLayout_15->addLayout(gridLayout_13, 2, 0, 1, 1);
+
+
+        gridLayout_16->addLayout(gridLayout_15, 0, 1, 1, 1);
+
+
+        gridLayout_18->addLayout(gridLayout_16, 1, 0, 1, 1);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 662, 21));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
+        comboBox->setItemText(0, QApplication::translate("MainWindow", "Linear Probing", nullptr));
+        comboBox->setItemText(1, QApplication::translate("MainWindow", "Displaced Probing", nullptr));
+        comboBox->setItemText(2, QApplication::translate("MainWindow", "Secondry Clustering", nullptr));
+        comboBox->setItemText(3, QApplication::translate("MainWindow", "Double Hashing", nullptr));
+
+        pushButton_2->setText(QApplication::translate("MainWindow", "Store", nullptr));
+        label->setText(QApplication::translate("MainWindow", "K for sc:", nullptr));
+        pushButton->setText(QApplication::translate("MainWindow", "Choose file", nullptr));
+        label_10->setText(QApplication::translate("MainWindow", "K for dp:", nullptr));
+        plainTextEdit_2->setPlainText(QString());
+        label_2->setText(QApplication::translate("MainWindow", "member id", nullptr));
+        label_3->setText(QApplication::translate("MainWindow", "name", nullptr));
+        label_4->setText(QApplication::translate("MainWindow", "surname", nullptr));
+        label_5->setText(QApplication::translate("MainWindow", "address", nullptr));
+        label_6->setText(QApplication::translate("MainWindow", "country", nullptr));
+        label_7->setText(QApplication::translate("MainWindow", "email", nullptr));
+        pushButton_3->setText(QApplication::translate("MainWindow", "Insert", nullptr));
+        label_11->setText(QApplication::translate("MainWindow", "total items", nullptr));
+        label_8->setText(QApplication::translate("MainWindow", "average probing", nullptr));
+        label_9->setText(QApplication::translate("MainWindow", "total probing    ", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
---
+++ Hash Table Storing/qfiledemo/mainwindow.cpp
@@ -0,0 +1,334 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFile>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QTextStream>
+#include <QtDebug>
+#include <QTime>
+#define PRIME 7
+const int T_S = 819;
+long long probe;
+QString file_name;
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+struct input{
+    QString id, name, surname, address, country, email;
+};
+class HashTableEntry {
+   public:
+      long long k;
+      struct input *v;
+      HashTableEntry(long long k, struct input *v) {
+         this->k= k;
+         this->v = v;
+      }
+};
+class HashMapTable {
+   public:
+      HashTableEntry *t[819];
+      QString showData[819];
+   public:
+      HashMapTable() {
+         for (int i = 0; i< T_S; i++) {
+            t[i] = NULL;
+         }
+      }
+      int HashFunc(long long k) {
+         return k % T_S;
+      }
+      int clusterFunc(long long k, int c){
+          return (HashFunc(k) + c)%T_S;
+      }
+      void Insert(long long k, struct input *v, int choice, int c) {
+          int h;
+          if(choice==0)
+              h = HashFunc(k);
+          else if(choice==1)
+              h = HashFunc(k);
+          else if(choice==2)
+              h = clusterFunc(k, c);
+
+         while (t[h] != NULL && t[h]->k != k) {
+             if(choice==0)
+                h = HashFunc(h + 1);
+             else if(choice==1)
+                 h = HashFunc(h + c);
+             else if(choice==2)
+                 h = clusterFunc(h + 1, c);
+            probe++;
+         }
+
+         if (t[h] != NULL)
+            delete t[h];
+         t[h] = new HashTableEntry(k, v);
+         QString enter;
+         enter = QString::number(h) + "        " + t[h]->v->id + "        " + t[h]->v->email + "\n";
+         showData[h]=enter;
+
+      }
+      ~HashMapTable() {
+         for (int i = 0; i < T_S; i++) {
+            if (t[i] != NULL)
+               delete t[i];
+         }
+      }
+};
+class doubleHashMapTable {
+   public:
+      HashTableEntry **t;
+      QString showData[819];
+   public:
+      doubleHashMapTable() {
+         t = new HashTableEntry * [T_S];
+         for (int i = 0; i< T_S; i++) {
+            t[i] = NULL;
+         }
+      }
+      int HashFunc(long long k) {
+         return k % T_S;
+      }
+      int doubleHashFunc(long long k){
+          return (PRIME - (k % PRIME));
+      }
+
+      void Insert(long long k, struct input *v) {
+         int h = HashFunc(k);
+
+         if (t[h] != NULL) {
+             int h2 = doubleHashFunc(k);
+             int i = 1;
+             while(1){
+                 int newIndex = (h + i * h2) % T_S;
+
+                 if (t[newIndex] == NULL) {
+                     delete t[newIndex];
+                     t[newIndex] = new HashTableEntry(k, v);
+                     QString enter;
+                     enter = QString::number(h) + "        " + t[h]->v->id + "        " + t[h]->v->email + "\n";
+                     showData[h]=enter;
+                     break;
+                 }
+                 i++;
+             }
+             probe++;
+         }
+         else{
+             delete t[h];
+             t[h] = new HashTableEntry(k, v);
+             QString enter;
+             enter = QString::number(h) + "        " + t[h]->v->id + "        " + t[h]->v->email + "\n";
+             showData[h]=enter;
+         }
+      }
+      ~doubleHashMapTable() {
+         for (int i = 0; i < T_S; i++) {
+            if (t[i] != NULL)
+               delete t[i];
+            delete[] t;
+         }
+      }
+};
+void MainWindow::on_pushButton_clicked()
+{
+    file_name = QFileDialog::getOpenFileName(this, "select files", "C:/");
+
+
+    QFile filename(file_name);
+    if(!filename.open(QFile::ReadOnly | QFile::Text)){
+        QMessageBox::warning(this, "title", "file not open");
+    }
+
+    QTextStream in(&filename);
+    QString text = in.readAll();
+    ui->plainTextEdit_2->setPlainText(text);
+    filename.close();
+}
+
+void delay()
+{
+    QTime dieTime= QTime::currentTime().addSecs(1000);
+    while (QTime::currentTime() < dieTime)
+        QCoreApplication::processEvents(QEventLoop::AllEvents, 100);
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    int choice = ui->comboBox->currentIndex();
+
+    if(choice==0) {
+        int c = ui->spinBox->value();
+        HashMapTable hash;
+        long long k;
+        int i=0;
+        probe = 0;
+        QString data = ui->plainTextEdit_2->toPlainText();
+        QStringList newdata = data.split("\n");
+        QString var;
+        QStringList newvar;
+
+        foreach (var, newdata) {
+            struct input v;
+            newvar = var.split(",",QString::SkipEmptyParts);
+            if(newvar.size()<6)
+                return;
+
+            v.id = newvar[0];
+            v.name = newvar[1];
+            v.surname = newvar[2];
+            v.address = newvar[3];
+            v.country = newvar[4];
+            v.email = newvar[5];
+            k = (v.id).toDouble();
+            hash.Insert(k, &v, choice, c);
+            i++;
+        }
+        ui->lineEdit_9->setText(QString::number(i));
+        QStringList temp;
+        for(int i=0;i<819;i++){
+            if(!hash.showData[i].isEmpty())
+                temp.append(hash.showData[i]);
+        }
+        QString show = temp.join("\n");
+        ui->plainTextEdit->setPlainText(show);
+        double avgprobe = (float)probe/i;
+        double totalprobe = probe;
+        ui->lineEdit_7->setText(QString::number(avgprobe));
+        ui->lineEdit_8->setText(QString::number(totalprobe));
+        delay();
+    }
+    else if(choice==1){
+        int c = ui->spinBox_2->value();
+        HashMapTable hash;
+        long long k;
+        int i=0;
+        probe = 0;
+        QString data = ui->plainTextEdit_2->toPlainText();
+        QStringList newdata = data.split("\n");
+        QString var;
+        QStringList newvar;
+        foreach (var, newdata) {
+            struct input v;
+            newvar = var.split(",");
+            if(!(newvar.size()<6)){
+                v.id = newvar[0];
+                v.name = newvar[1];
+                v.surname = newvar[2];
+                v.address = newvar[3];
+                v.country = newvar[4];
+                v.email = newvar[5];
+                k = (v.id).toDouble();
+                hash.Insert(k, &v, choice, c);
+                i++;
+            }
+        }
+        ui->lineEdit_9->setText(QString::number(i));
+        QStringList temp;
+        for(int i=0;i<819;i++){
+            if(!hash.showData[i].isEmpty())
+                temp.append(hash.showData[i]);
+        }
+        QString show = temp.join("\n");
+        ui->plainTextEdit->setPlainText(show);
+        double avgprobe = (float)probe/i;
+        double totalprobe = probe;
+        ui->lineEdit_7->setText(QString::number(avgprobe));
+        ui->lineEdit_8->setText(QString::number(totalprobe));
+        delay();
+    }
+    else if(choice==2){
+        int c = ui->spinBox->value();
+        HashMapTable hash;
+        long long k;
+        int i=0;
+        probe = 0;
+        QString data = ui->plainTextEdit_2->toPlainText();
+        QStringList newdata = data.split("\n");
+        QString var;
+        QStringList newvar;
+        foreach (var, newdata) {
+            struct input v;
+            newvar = var.split(",");
+            if(!(newvar.size()<6)){
+                v.id = newvar[0];
+                v.name = newvar[1];
+                v.surname = newvar[2];
+                v.address = newvar[3];
+                v.country = newvar[4];
+                v.email = newvar[5];
+                k = (v.id).toDouble();
+                hash.Insert(k, &v, choice, c);
+                i++;
+            }
+        }
+        ui->lineEdit_9->setText(QString::number(i));
+        QStringList temp;
+        for(int i=0;i<819;i++){
+            if(!hash.showData[i].isEmpty())
+                temp.append(hash.showData[i]);
+        }
+        QString show = temp.join("\n");
+        ui->plainTextEdit->setPlainText(show);
+        double avgprobe = (float)probe/i;
+        double totalprobe = probe;
+        ui->lineEdit_7->setText(QString::number(avgprobe));
+        ui->lineEdit_8->setText(QString::number(totalprobe));
+        delay();
+    }
+    else if(choice==3){
+        doubleHashMapTable hash;
+        long long k;
+        int i=0;
+        probe = 0;
+        QString data = ui->plainTextEdit_2->toPlainText();
+        QStringList newdata = data.split("\n");
+        QString var;
+        QStringList newvar;
+        foreach (var, newdata) {
+            struct input v;
+            newvar = var.split(",");
+            if(!(newvar.size()<6)){
+                v.id = newvar[0];
+                v.name = newvar[1];
+                v.surname = newvar[2];
+                v.address = newvar[3];
+                v.country = newvar[4];
+                v.email = newvar[5];
+                k = (v.id).toDouble();
+                hash.Insert(k, &v);
+                i++;
+            }
+        }
+        ui->lineEdit_9->setText(QString::number(i));
+        QStringList temp;
+        for(int i=0;i<819;i++){
+            if(!hash.showData[i].isEmpty())
+                temp.append(hash.showData[i]);
+        }
+        QString show = temp.join("\n");
+        ui->plainTextEdit->setPlainText(show);
+        double avgprobe = (float)probe/i;
+        double totalprobe = probe;
+        ui->lineEdit_7->setText(QString::number(avgprobe));
+        ui->lineEdit_8->setText(QString::number(totalprobe));
+        delay();
+    }
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    QString add = ui->lineEdit->text()+","+ui->lineEdit_2->text()+","+ui->lineEdit_3->text()+","+ui->lineEdit_4->text()+","+ui->lineEdit_5->text()+","+ui->lineEdit_6->text()+"\n";
+    QString data = ui->plainTextEdit_2->toPlainText()+add;
+    ui->plainTextEdit_2->setPlainText(data);
+}
